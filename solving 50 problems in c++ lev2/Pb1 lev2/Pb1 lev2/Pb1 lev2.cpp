de469e92d3167e9642c7acc94ec3e300b646b12f
Majd Flaifel <majdflaifle@gmail.com>
2025-08-06 22:48:55 +0300

    Reload problem1

---
+++ solving 50 problems in c++ lev2/Pb1 lev2/Pb1 lev2/Pb1 lev2.cpp
@@ -0,0 +1,14 @@
+// Pb1 lev2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+using namespace std;
+void names() {
+	cout << "majd\n";
+}
+
+int main()
+{ 
+	names();
+
+}
